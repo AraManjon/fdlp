84396b5e13434b73732198a6f1f7aacee675bff6
Ara <ara.manjoncb@gmail.com>
2022-02-28 00:58:13 +0100

    feat: add movies exercise

---
+++ callback-exercise/movies/index.js
@@ -0,0 +1,31 @@
+import { films } from './films.js'
+console.log(films)
+// show all films titles
+function showTitleFilms () {
+}
+showTitleFilms()
+
+// create new array with imdbRating to number 
+function changeRatingToNumber () {
+}
+changeRatingToNumber()
+
+// order films by year
+function orderFilmsByYear () {
+}
+orderFilmsByYear()
+
+// show comming soon films
+function showCommingSoonFilms () {
+}
+showCommingSoonFilms()
+
+// find some serie with genere Action
+function findSomeSerie () {
+}
+findSomeSerie()
+
+// reduce imdbRating and calculate average
+function averageRating () {
+}
+averageRating()
