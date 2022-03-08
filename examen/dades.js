197da1feda7e018988b7adbfaaedea5d30772624
Ara <ara.manjoncb@gmail.com>
2022-03-08 22:01:45 +0100

    added exam

---
+++ examen/dades.js
@@ -0,0 +1,40 @@
+const treballadors = [
+   {
+      nom : "Ana",
+      cognom: "Puig",
+      carrec: "venedor(a)",
+      disponibilitatTrasllat : true
+   },
+   {
+      nom : "Lluis",
+      cognom: "Soto",
+      carrec: "venedor(a)",
+      disponibilitatTrasllat : false
+   },
+   {
+      nom : "Cristina",
+      cognom: "Tomas",
+      carrec: "administratiu(a)",
+      disponibilitatTrasllat : false
+   },
+   {
+      nom : "Andreu",
+      cognom: "Ricart",
+      carrec: "venedor(a)",
+      disponibilitatTrasllat : true
+   },
+   {
+      nom : "Pep",
+      cognom: "Vila",
+      carrec: "venedor(a)",
+      disponibilitatTrasllat : false
+   },
+   {
+      nom : "Núria",
+      cognom: "Roure",
+      carrec: "administratiu(a)",
+      disponibilitatTrasllat : true
+   },
+
+]
+
